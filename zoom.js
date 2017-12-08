1941310c84e174ff8da726e80e12e12bad2ef220
Tomasz Sapiński <tomasz.sapinski@lhsystems.com>
2017-12-08 09:59:26 +0100

    added single touch support

--- zoom.js
+++ zoom.js
@@ -4,86 +4,86 @@
 
 /**
  * Multiply Scalar with Vector returns a Vector.
- * 
+ *
  * @param {number} l scalar to multiply with
  * @param {Array<number>} x 2D vector.
  * @return {Array<number>}
  */
-var scmult = function(l, x) {
-    return [ l * x[0], l * x[1] ];
+var scmult = function (l, x) {
+    return [l * x[0], l * x[1]];
 };
 
 /**
  * Adding two vectors is another vector.
- * 
+ *
  * @param {Array<number>} a 2D vector.
  * @param {Array<number>} b 2D vector.
  * @return {Array<number>} Sum vector.
  */
-var vcadd = function(a, b) {
-    return [ a[0] + b[0], a[1] + b[1] ];
+var vcadd = function (a, b) {
+    return [a[0] + b[0], a[1] + b[1]];
 };
 
 /**
  * Subtracting two vectors is another vector.
- * 
+ *
  * @param {Array<number>} a 2D vector.
  * @param {Array<number>} b 2D vector.
  * @return {Array<number>} Difference vector.
  */
-var minus = function(a, b) {
-    return [ a[0] - b[0], a[1] - b[1] ];
+var minus = function (a, b) {
+    return [a[0] - b[0], a[1] - b[1]];
 };
 
 /**
  * Dot product of two vectors is scalar.
- * 
+ *
  * @param {Array<number>} a 2D vector.
  * @param {Array<number>} b 2D vector.
  * @return {number} scalar inner product.
  */
-var dot = function(a, b) {
+var dot = function (a, b) {
     return a[0] * b[0] + a[1] * b[1];
 };
 
 /**
  * Exterior Product of two vectors is a pseudoscalar.
- * 
+ *
  * @param {Array<number>} a 2D vector.
  * @param {Array<number>} b 2D vector.
  * @return {number} psuedo-scalar exterior product.
  */
-var wedge = function(a, b) {
+var wedge = function (a, b) {
     return a[0] * b[1] - a[1] * b[0];
 };
 
 /**
  * Apply Matrix on Vector returns a Vector.
- * 
+ *
  * @param {Array<Array<number>>} A 2x2 Matrix
  * @param {Array<number>} x 2D vector.
  * @return {Array<number>} 2D vector linear product.
  */
-var apply = function(A, x) {
+var apply = function (A, x) {
     return vcadd(scmult(x[0], A[0]), scmult(x[1], A[1]));
 };
 
 /**
  * Multiply two matrices.
- * 
+ *
  * @param {Array<Array<number>>} A 2x2 Matrix
  * @param {Array<Array<number>>} B 2x2 Matrix
  * @return {Array<Array<number>>} A 2x2 Matrix
  */
-var mult = function(A, B) {
-    return [ apply(A, B[0]), apply(A, B[1]) ];
+var mult = function (A, B) {
+    return [apply(A, B[0]), apply(A, B[1])];
 };
 
 /**
  * Represents a transform operation, Ax + b
- * 
+ *
  * @constructor
- * 
+ *
  * @param {Array<Array<number>>} A 2x2 Matrix.
  * @param {Array<number>} b 2D scalar.
  */
@@ -94,60 +94,60 @@ function Transform(A, b) {
 
 /**
  * Given CSS Transform representation of the class.
- * @return {string} CSS 2D Transform. 
+ * @return {string} CSS 2D Transform.
  */
-Transform.prototype.css = function() {
+Transform.prototype.css = function () {
     var A = this.A;
     var b = this.b;
     return 'matrix(' + A[0][0] + ',' + A[0][1] + ',' + A[1][0] + ',' + A[1][1] +
-            ',' + b[0] + ',' + b[1] + ')';
+        ',' + b[0] + ',' + b[1] + ')';
 };
 
 /**
- * Multiply two transforms. 
- * Defined as 
+ * Multiply two transforms.
+ * Defined as
  *  (T o U) (x) = T(U(x))
- * 
+ *
  * Derivation:
- *  T(U(x)) 
- *   = T(U.A(x) + U.b) 
+ *  T(U(x))
+ *   = T(U.A(x) + U.b)
  *   = T.A(U.A(x) + U.b)) + T.b
- *   = T.A(U.A(x)) + T.A(U.b) + T.b 
- * 
- * @param {Transform} T 
- * @param {Transform} U 
+ *   = T.A(U.A(x)) + T.A(U.b) + T.b
+ *
+ * @param {Transform} T
+ * @param {Transform} U
  * @return {Transform} T o U
  */
-var cascade = function(T, U) {
+var cascade = function (T, U) {
     return new Transform(mult(T.A, U.A), vcadd(apply(T.A, U.b), T.b));
 };
 
 /**
  * Creates the default rotation matrix
- * 
+ *
  * @param {number} c x-projection (r cos(theta))
  * @param {number} s y-projection (r sin(theta))
  * @return {Array<Array<number>>} Rotation matrix.
  */
-var rotate = function(c, s) {
-    return [ [ c, s], [-s, c] ];
+var rotate = function (c, s) {
+    return [[c, s], [-s, c]];
 };
 
 /**
  * Returns matrix that transforms vector a to vector b.
- * 
+ *
  * @param {Array<number>} a 2D vector.
  * @param {Array<number>} b 2D vector.
  * @return {Array<Array<number>>} Rotation + Scale matrix
  */
-var rotscale = function(a, b) {
+var rotscale = function (a, b) {
     var alen = dot(a, a);
     var sig = dot(a, b);
     var del = wedge(a, b);
-    return rotate( sig / alen, del / alen);
+    return rotate(sig / alen, del / alen);
 };
 
-var justscale = function(a, b) {
+var justscale = function (a, b) {
     var alen = Math.sqrt(dot(a, a));
     var blen = Math.sqrt(dot(b, b));
     var scale = blen / alen;
@@ -159,14 +159,14 @@ var justscale = function(a, b) {
  * points to a new pair of destination points. If rotate it is false
  * then it won't be maintaining the transfer precisely, but will only
  * do scaling part of it.
- * 
+ *
  * @param {Array<Array<number>>} s two source points.
  * @param {Array<Array<number>>} d two destination points.
  * @param {Boolean} rotate true - rotate; else scale.
- * 
- * @return {Transform} that moves point 's' to point 'd' 
- */ 
-var zoom = function(s, d, rotate) {
+ *
+ * @return {Transform} that moves point 's' to point 'd'
+ */
+var zoom = function (s, d, rotate) {
     // Source vector.
     var a = minus(s[1], s[0]);
     // Destination vector.
@@ -184,13 +184,13 @@ var zoom = function(s, d, rotate) {
 
 /**
  * Weighted average of two vectors.
- * 
+ *
  * @param {Array<number>} u 2D vector.
  * @param {Array<number>} v 2D vector.
  * @param {number} progress (from 0 to 1)
- * @return {Array<number>} (1-p) u + (p) v 
+ * @return {Array<number>} (1-p) u + (p) v
  */
-var avgVector = function(u, v, progress) {
+var avgVector = function (u, v, progress) {
     var u1 = scmult(1 - progress, u);
     var v1 = scmult(progress, v);
     return vcadd(u1, v1);
@@ -198,14 +198,14 @@ var avgVector = function(u, v, progress) {
 
 /**
  * Weighted average of two vectors.
- * 
+ *
  * @return {Array<Array<number>>} A 2D matrix.
  * @return {Array<Array<number>>} B 2D matrix.
  * @param {number} progress (from 0 to 1)
- * @return {Array<Array<number>>} (1-p) A + (p) B 
+ * @return {Array<Array<number>>} (1-p) A + (p) B
  */
-var avgMatrix = function(A, B, progress) {
-    return [ avgVector(A[0], B[0], progress),  avgVector(A[1], B[1], progress) ];
+var avgMatrix = function (A, B, progress) {
+    return [avgVector(A[0], B[0], progress), avgVector(A[1], B[1], progress)];
 };
 
 
@@ -214,9 +214,9 @@ var avgMatrix = function(A, B, progress) {
  * @param {Transform} Z Source Transform
  * @param {Transform} I Destination Transform
  * @param {number} progress (from 0 to 1)
- * @return {Transform} (1-p) Z + (p) I 
+ * @return {Transform} (1-p) Z + (p) I
  */
-Transform.avg = function(Z, I, progress) {
+Transform.avg = function (Z, I, progress) {
     return new Transform(avgMatrix(Z.A, I.A, progress), avgVector(Z.b, I.b, progress));
 };
 
@@ -224,12 +224,12 @@ var identity = new Transform([[1, 0], [0, 1]], [0, 0]);
 
 /**
  * Gives a default value for an input object.
- * 
+ *
  * @param {Object} param input parameter, may be undefined
  * @param {Object} val returned if param is undefined.
  * @return {Object}
  */
-var defaults = function(param, val) {
+var defaults = function (param, val) {
     return (param == undefined) ? val : param;
 };
 
@@ -237,12 +237,12 @@ var defaults = function(param, val) {
  * Method to override json config objects with default
  * values. If undefined in cfg corresponding value from
  * cfg_def will be picked.
- * 
+ *
  * @param {Object} cfg input parameter config.
  * @param {Object} cfg_def default fallbacks.
  * @return {Object} new config
  */
-var default_config = function(cfg, cfg_def) {
+var default_config = function (cfg, cfg_def) {
     var new_cfg = defaults(cfg, {})
     for (k in cfg_def) {
         new_cfg[k] = defaults(new_cfg[k], cfg_def[k])
@@ -268,56 +268,65 @@ function Zoom(elem, config, wnd) {
     this.srcCoords = [0, 0];
     this.destCoords = [0, 0];
     var me = this;
-    
+
     this.config = default_config(config, {
-        "pan" : false,
-        "rotate" : true
+        "pan": false,
+        "rotate": true
     });
 
     this.wnd = wnd || window;
 
     elem.style['transform-origin'] = '0 0';
 
-    var getCoordsDouble = function(t) {
+    var getCoordsDouble = function (t) {
         var oX = elem.offsetLeft;
-        var oY = elem.offsetTop; 
-        return [ 
+        var oY = elem.offsetTop;
+        return [
             [t[0].pageX - oX, t[0].pageY - oY],
-            [t[1].pageX - oX, t[1].pageY - oY] 
+            [t[1].pageX - oX, t[1].pageY - oY]
         ];
     };
 
-    var getCoordsSingle = function(t) {
+    var getCoordsSingle = function (t) {
         var oX = elem.offsetLeft;
-        var oY = elem.offsetTop; 
+        var oY = elem.offsetTop;
         var x = t[0].pageX - oX;
         var y = t[0].pageY - oY;
-        return [ 
+        return [
             [x, y],
-            [x + 1, y + 1] 
+            [x + 1, y + 1]
         ];
     };
 
-    var getCoords = function(t) {
+    var getCoords = function (t) {
         return t.length > 1 ? getCoordsDouble(t) : getCoordsSingle(t);
     };
 
-    var setSrcAndDest = function(touches){
+    var setSrcAndDest = function (touches) {
         me.srcCoords = getCoords(touches);
         me.destCoords = me.srcCoords;
     };
 
-    var setDest = function(touches){
+    var setDest = function (touches) {
         me.destCoords = getCoords(touches);
     };
 
-    var handleTouchEvent = function(cb) {
-        return function(evt) {
-            evt.preventDefault();
-            if (me.isAnimationRunning){
-                return false;
-            }            
+    var handleTouchEvent = function (cb) {
+        return function (evt) {
             var touches = evt.touches;
+
+            if (touches.length > 1 || evt.type === "touchmove") {
+                evt.preventDefault();
+            }
+
+            me.wnd.setTimeout(function () {
+                evt.preventDefault();
+            }, 300)
+
+
+            if (me.isAnimationRunning) {
+                return false;
+            }
             if (!touches) {
                 return false;
             }
@@ -325,9 +334,9 @@ function Zoom(elem, config, wnd) {
         };
     };
 
-    var handleZoom = handleTouchEvent(function(touches) {
+    var handleZoom = handleTouchEvent(function (touches) {
         var numOfFingers = touches.length;
-        if (numOfFingers != me.curTouch){
+        if (numOfFingers != me.curTouch) {
             me.curTouch = numOfFingers;
             me.finalize();
             if (numOfFingers != 0) {
@@ -338,16 +347,16 @@ function Zoom(elem, config, wnd) {
             me.previewZoom();
         }
     });
-    
-    var handleTouchStart = handleTouchEvent(function(touches) {
+
+    var handleTouchStart = handleTouchEvent(function (touches) {
         if (touches.length === 1) {
             if (me.mayBeDoubleTap != null) {
                 me.wnd.clearTimeout(me.mayBeDoubleTap);
                 me.reset();
                 me.mayBeDoubleTap = null;
             } else {
-                me.mayBeDoubleTap = me.wnd.setTimeout(function() {
-                    me.mayBeDoubleTap = null;                    
+                me.mayBeDoubleTap = me.wnd.setTimeout(function () {
+                    me.mayBeDoubleTap = null;
                 }, 300);
             }
         }
@@ -359,26 +368,26 @@ function Zoom(elem, config, wnd) {
     elem.parentNode.addEventListener('touchend', handleZoom);
 }
 
-Zoom.prototype.previewZoom = function() {
+Zoom.prototype.previewZoom = function () {
     var additionalZoom = zoom(this.srcCoords, this.destCoords, this.config.rotate);
     this.resultantZoom = cascade(additionalZoom, this.activeZoom);
     this.repaint();
 };
 
-Zoom.prototype.setZoom = function(newZoom) {
+Zoom.prototype.setZoom = function (newZoom) {
     this.resultantZoom = newZoom;
     this.repaint();
 };
 
-Zoom.prototype.finalize = function() {
+Zoom.prototype.finalize = function () {
     this.activeZoom = this.resultantZoom;
 };
 
-Zoom.prototype.repaint = function() {
+Zoom.prototype.repaint = function () {
     this.elem.style.transform = this.resultantZoom.css();
 };
 
-Zoom.prototype.reset = function() {
+Zoom.prototype.reset = function () {
     if (this.wnd.requestAnimationFrame) {
         this.isAnimationRunning = true;
         var Z = this.activeZoom;
@@ -386,11 +395,11 @@ Zoom.prototype.reset = function() {
 
         var me = this;
 
-        var step = function(time) {
-            if (!startTime) { 
-                startTime =  time;
+        var step = function (time) {
+            if (!startTime) {
+                startTime = time;
             }
-            var progress = (time - startTime)/100;
+            var progress = (time - startTime) / 100;
             if (progress >= 1) {
                 me.setZoom(identity);
                 me.isAnimationRunning = false;
